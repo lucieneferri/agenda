daffbeb709db85fb47af57c653883dd86df56c14
lucieneferri <78495231+lucieneferri@users.noreply.github.com>
2021-09-20 10:40:49 -0300

    autenticacao, login e decoradores

--- agenda/urls.py
+++ agenda/urls.py
@@ -20,5 +20,7 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
-    path('', views.index)
+    path('', views.index),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login)
 ]
